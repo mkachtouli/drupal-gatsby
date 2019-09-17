9a6ac267b0d92293093635d9bdcbaed3e311bfd7
Mohamed ELKACHTOULI <mohamed.elkachtouli@gmail.com>
2019-09-17 15:17:12 +0100

    Workbox

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,7 +42,14 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/news/`, `/news/*`],
+        appendScript: require.resolve(`src/custom-sw-code.js`),
+        importWorkboxFrom: `cdn`,
+      },
+    },
   ],
 }
  
